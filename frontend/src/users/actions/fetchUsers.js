fc279a5f816a1a349832bc06f930bfc5ee49c950
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-05 07:30:14 -0500

    updated

--- frontend/src/users/actions/fetchUsers.js
+++ frontend/src/users/actions/fetchUsers.js
@@ -1,7 +1,8 @@
 import indexUrl from '../../indexUrl'
+import { userActions } from '../UserImport';
 const fetchUsers = () => dispatch => {
-  dispatch({type: "START_ADDING_USERS_REQUEST"})
+  dispatch(userActions.initUsers)
   fetch(indexUrl + 'users').then(resp => resp.json()).then(users =>
-    dispatch({type: "ADD_USERS", users}))
+    dispatch({type: "ADD_USERS", users})).catch(error => console.log(error));
 };
 export default fetchUsers
