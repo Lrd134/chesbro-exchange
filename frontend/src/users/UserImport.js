ba1e9a61886b30bfd260777d5dfbefedb1b142ea
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-05 02:14:28 -0500

    added userActions

--- frontend/src/users/UserImport.js
+++ frontend/src/users/UserImport.js
@@ -1,6 +1,6 @@
 import UsersContainer from "./containers/UsersContainer";
 import UserForm from "./components/UserForm";
 import UserReducer from "./reducers/UserReducer";
+import UserActions from './actions/UserActions';
 
-
-export { UsersContainer, UserForm, UserReducer }
+export { UsersContainer, UserForm, UserReducer, UserActions }
