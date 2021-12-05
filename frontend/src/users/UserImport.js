f1394ced2a1f9d59696020c6c78e60039e63bae5
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-05 02:32:39 -0500

    updated imports

--- frontend/src/users/UserImport.js
+++ frontend/src/users/UserImport.js
@@ -2,5 +2,6 @@ import UsersContainer from "./containers/UsersContainer";
 import UserForm from "./components/UserForm";
 import UserReducer from "./reducers/UserReducer";
 import UserActions from './actions/UserActions';
+import User from './components/User';
 
-export { UsersContainer, UserForm, UserReducer, UserActions }
+export { UsersContainer, UserForm, UserReducer, UserActions, User }
--- frontend/src/users/containers/UsersContainer.js
+++ frontend/src/users/containers/UsersContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import User from '../components/User';
+import { User } from '../UserImport';
 import { connect } from 'react-redux';
 class UsersContainer extends Component {
   
