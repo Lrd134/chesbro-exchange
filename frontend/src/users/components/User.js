b1bb570ed43ac4e359995f557da4eb5dec483b4c
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-08 18:35:14 -0500

    properly displays

--- frontend/src/users/components/User.js
+++ frontend/src/users/components/User.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-const User = () => {
-  let params = useParams();
-  return (
-    <li>{params.userId}</li>
-  )  
+import { connect } from 'react-redux';
+const User = ({users}) => {
+
+
+    const id = parseInt(useParams().userId, 10)
+    const user = users.find(user => user.id === id)
+    if (user)
+      return (
+        <div>
+          <h1>{user.name}</h1>
+          <h2>{user.id}</h2>
+        </div>
+      )
+    else
+      return (
+        <div>
+          No User Found With ID: {id}
+        </div>
+      )
 }
 
+const mapStateToProps = ({users}) => {
+  return {
+    users: users.users
+  }
 
-export default User;
+}
+
+export default connect(mapStateToProps)(User);
