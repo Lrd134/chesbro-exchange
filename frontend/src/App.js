4aa71b71b2d3c804507d67e3502c913b4537368f
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-08 18:33:25 -0500

    handle api data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { fetchUsers } from './users/UserImport';
 
 import { Outlet, Link } from "react-router-dom";
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchUsers();
+  }
   render() {
     return (
       <div className="App">
         <Link to="/users">Users</Link>
         <Link to="/signup">Signup</Link>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
         <Outlet />
       </div>
     );
   }
 }
-
-export default connect()(App);
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchUsers: () => fetchUsers(dispatch)
+  }
+}
+export default connect(null, mapDispatchToProps)(App);
