8f84507d00cd344e9d616cc30e0fc5671ce1648b
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-08 18:33:43 -0500

    refactored benign code

--- frontend/src/users/actions/fetchUsers.js
+++ frontend/src/users/actions/fetchUsers.js
@@ -1,6 +1,6 @@
 import indexUrl from '../../indexUrl'
 import { userActions } from '../UserImport';
-const fetchUsers = () => dispatch => {
+const fetchUsers = dispatch => {
   dispatch(userActions.initUsers)
   fetch(indexUrl + 'users').then(resp => resp.json()).then(users =>
     dispatch({type: "ADD_USERS", users})).catch(error => console.log(error));
