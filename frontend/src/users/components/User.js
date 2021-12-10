af8112e48b1d0d6ab5194a2e5fd9d22e997b50ff
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-10 13:54:13 -0500

    refactored user

--- frontend/src/users/components/User.js
+++ frontend/src/users/components/User.js
@@ -1,39 +1,11 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import { connect } from 'react-redux';
-import {  UserForm } from '../UserImport';
-const User = ({users}) => {
 
-    const id = parseInt(useParams().userId, 10)
-    if (window.sessionStorage.getItem('logged_in'))
-      return (
-        <div>
-          <UserForm />
-        </div>
-      )
-    else {
-      const user = users.find(user => user.id === id)
-      if (user)
-        return (
-          <div>
-            <h1>{user.name}</h1>
-            <h2>{user.id}</h2>
-          </div>
-        )
-      else
-        return (
-          <div>
-            No User Found With ID: {id}
-          </div>
-        )
-    }
+const User = ({user}) => {
+  return (
+    <li key={user.id}>
+      {user.name}
+    </li>
+  )
 }
 
-const mapStateToProps = ({users}) => {
-  return {
-    users: users.users
-  }
-
-}
-
-export default connect(mapStateToProps)(User);
+export default User;
