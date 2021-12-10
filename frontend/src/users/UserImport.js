b7bc57bdb93522a96041ab6e1a9cd1fa615a2848
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-10 01:49:38 -0500

    updated exports

--- frontend/src/users/UserImport.js
+++ frontend/src/users/UserImport.js
@@ -3,6 +3,6 @@ import UserForm from "./components/UserForm";
 import userReducer from "./reducers/userReducer";
 import userActions from './actions/userActions';
 import User from './components/User';
-import { getUsers, newUser } from './actions/userFetch';
-
-export { UsersContainer, UserForm, userReducer, userActions, User, getUsers, newUser }
+import { getUsers, newUser, updateUser } from './actions/userFetch';
+import UserOverlay from './containers/UserOverlay';
+export { UsersContainer, UserForm, userReducer, userActions, User, getUsers, newUser, updateUser, UserOverlay }
