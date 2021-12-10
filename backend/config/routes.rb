e97ccd05f81ae8eef381668232bbffb1187a15e4
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-10 14:32:34 -0500

    reverted routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :tokens
-
-  resources :users do
-    resources :balances
-  end
+  resources :balances
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
