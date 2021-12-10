77720569e998c80982fa4f66737ed04a9ae024ba
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-10 01:48:51 -0500

    added logic for logged in

--- frontend/src/users/components/User.js
+++ frontend/src/users/components/User.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
+import {  UserForm } from '../UserImport';
 const User = ({users}) => {
 
-
     const id = parseInt(useParams().userId, 10)
-    const user = users.find(user => user.id === id)
-    if (user)
+    if (window.sessionStorage.getItem('logged_in'))
       return (
         <div>
-          <h1>{user.name}</h1>
-          <h2>{user.id}</h2>
+          <UserForm />
         </div>
       )
-    else
-      return (
-        <div>
-          No User Found With ID: {id}
-        </div>
-      )
+    else {
+      const user = users.find(user => user.id === id)
+      if (user)
+        return (
+          <div>
+            <h1>{user.name}</h1>
+            <h2>{user.id}</h2>
+          </div>
+        )
+      else
+        return (
+          <div>
+            No User Found With ID: {id}
+          </div>
+        )
+    }
 }
 
 const mapStateToProps = ({users}) => {
