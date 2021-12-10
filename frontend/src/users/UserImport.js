6dc00b74b9f904b4ba668163880a9cf7af846787
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-10 13:54:35 -0500

    removed UserOverlay

--- frontend/src/users/UserImport.js
+++ frontend/src/users/UserImport.js
@@ -4,5 +4,4 @@ import userReducer from "./reducers/userReducer";
 import userActions from './actions/userActions';
 import User from './components/User';
 import { getUsers, newUser, updateUser } from './actions/userFetch';
-import UserOverlay from './containers/UserOverlay';
-export { UsersContainer, UserForm, userReducer, userActions, User, getUsers, newUser, updateUser, UserOverlay }
+export { UsersContainer, UserForm, userReducer, userActions, User, getUsers, newUser, updateUser }
