3cd61767778ceb84e16336ba31d0c2c18d76c25b
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-10 14:44:52 -0500

    fetching tokens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getUsers } from './users/UserImport';
-
+import { getTokens } from './tokens/TokenImport';
 import { Outlet, Link } from "react-router-dom";
 import { getBalances } from './balances/BalanceImport';
 class App extends Component {
   componentDidMount() {
     this.props.getUsers();
     this.props.getBalances();
+    this.props.getTokens();
   }
   render() {
     return (
@@ -17,7 +18,9 @@ class App extends Component {
 
         <Link to="/users">Users</Link>
         <Link to="/balances">Balances</Link>
+        <Link to="/tokens">Tokens</Link>
         {window.sessionStorage.getItem("loggedIn") ? <Link to="/me">Profile</Link> : <Link to="/signup">Signup</Link>}
+        {window.sessionStorage.getItem("loggedIn") ? null : <Link to="/login">Login</Link>}
         <Outlet />
       </div>
     );
@@ -26,7 +29,8 @@ class App extends Component {
 const mapDispatchToProps = dispatch => {
   return {
     getUsers: () => getUsers(dispatch),
-    getBalances: () => getBalances(dispatch)
+    getBalances: () => getBalances(dispatch),
+    getTokens: () => getTokens(dispatch)
   }
 }
 export default connect(null, mapDispatchToProps)(App);
