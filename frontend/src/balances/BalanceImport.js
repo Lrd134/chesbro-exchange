3f05ed66c2e31d545503f1f8786bb95560325c56
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-10 13:54:51 -0500

    updated balance exports

--- frontend/src/balances/BalanceImport.js
+++ frontend/src/balances/BalanceImport.js
@@ -1,4 +1,4 @@
-import BalanceActions from './actions/BalanceActions';
+import balanceActions from './actions/balanceActions';
 import BalanceReducer from './reducers/BalanceReducer';
-
-export { BalanceActions, BalanceReducer }
+import { getBalances, newBalance, updateBalance } from './actions/balanceFetch'
+export { balanceActions, BalanceReducer, getBalances, newBalance, updateBalance }
