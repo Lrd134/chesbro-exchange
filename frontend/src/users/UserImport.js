aa6104e27b456089ab191051a0252df58012ad34
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-10 15:41:51 -0500

    updated imports

--- frontend/src/users/UserImport.js
+++ frontend/src/users/UserImport.js
@@ -5,4 +5,7 @@ import userActions from './actions/userActions';
 import User from './components/User';
 import { getUsers, newUser, updateUser, loginUser } from './actions/userFetch';
 import LoginForm from './components/LoginForm';
-export { UsersContainer, UserForm, userReducer, userActions, User, getUsers, newUser, updateUser, LoginForm, loginUser }
+import login from './helpers/login';
+import redirect from '../helpers/redirect'
+import Logout from './components/Logout';
+export { UsersContainer, UserForm, userReducer, userActions, User, getUsers, newUser, updateUser, LoginForm, loginUser, login, redirect, Logout }
