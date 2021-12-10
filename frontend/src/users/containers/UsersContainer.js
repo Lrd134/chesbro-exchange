45480b716893dce931d3a87a683126d62a2c7d3d
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-10 13:53:23 -0500

    updated renderUsers

--- frontend/src/users/containers/UsersContainer.js
+++ frontend/src/users/containers/UsersContainer.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-
+import { User } from '../UserImport';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 class UsersContainer extends Component {
   
   render() {
-    const renderUsers = () => this.props.users.map(user => <Link to={`${user.id}`} key={user.id}>{user.name}<br></br></Link>)
+    const renderUsers = () => this.props.users.map(user => <User user={user}/>)
     return (
       <div className="user-container">
         <ul className="user-list">
