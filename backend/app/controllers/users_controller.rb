3954ac8bef34fbda2e0b6bb602930872d4d5debe
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-10 15:16:15 -0500

    can login with name

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = User.find_by_any_params(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :balances
+  def self.find_by_any_params params
+    params.match(/\D/) ? self.find_by_name(params) : self.find(params)
+  end
 end
