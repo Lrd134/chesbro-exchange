9361532a28b722b10c26e678b85e9a6263db75df
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-11 13:45:57 -0500

    rewrote user controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,58 +1,38 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: %i[show destroy update]
 
-  # GET /users
   def index
-    @users = User.all
-
-    render json: @users
+    users = User.all
+    render json: users
   end
 
-  # GET /users/1
   def show
     render json: @user
   end
 
-  # POST /users
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    @user = User.create(user_params)
+    render json: @user
   end
 
-  # PATCH/PUT /users/1
   def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    @user = User.update(user_params)
+    render json: @user
   end
 
-  # DELETE /users/1
   def destroy
-    if @user
-      @message = {:message => "Deleted Successfully."}
-      @user.destroy
-      render json: @message.to_json
-    else
-      @message = {:message => "User not found."} 
-      render json: @message.to_json
-    end
+    @user.destroy
+    message = { :message => "Destroyed Successfully." }
+    render json: message
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find_by_any_params(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:name, :email)
+    end
+
+    def set_user
+      @user = User.find_by_any_params(params[:id])
+      @user ? @user : (render json: { :message => "User not found." })
     end
 end
