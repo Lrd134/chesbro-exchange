fb8ba8aef839bde18de8571a3e3ba43da4ce8421
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-11 13:19:42 -0500

    added edit button

--- frontend/src/users/containers/UserContainer.js
+++ frontend/src/users/containers/UserContainer.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { login,
-        userActions as Actions,
-        deleteUser } from '../UserImport';
+        deleteUser,
+        updateUser,
+        User, 
+        UserForm } from '../UserImport';
 
 
 class UserContainer extends React.Component { 
+  state= {
+    name: "",
+    email: "",
+    editing: false
 
+  }
+  handleDelete = event => {
+    this.props.deleteUser();
+  }
+  openEditForm = () => {
+    this.setState({...this.state, editing: true })
+  }
+  render() {
+    const { user } = login.current;
+    return (
+      <div className="profile">
+        <ul>
+          <User key={user.id} user={user} />
+          {this.state.editing ? <UserForm /> : null}
+        </ul>
+        <button onClick={this.handleDelete} >Delete {user.name}</button>
+        <button onClick={this.openEditForm} >Edit {user.name}</button>
+      </div>
+    )
+  }
 }
-mapDispatchToProps = dispatch => ({ deleteUser: deleteUser(dispatch, login.current)})
-export default connect()(UserContainer);
+const mapDispatchToProps = dispatch => ({ deleteUser: () => deleteUser(dispatch, login.current),
+                                          updateUser: () => updateUser(dispatch, login.current) })
+export default connect(null, mapDispatchToProps)(UserContainer);
