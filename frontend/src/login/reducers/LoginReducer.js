bbfd8c1d82a36372e500ebb069643a55f96b0c0b
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-11 17:37:35 -0500

    handles requesting

--- frontend/src/login/reducers/LoginReducer.js
+++ frontend/src/login/reducers/LoginReducer.js
@@ -5,12 +5,12 @@ const LoginReducer = (state = {
   requesting: false,
 }, action) => {
   switch(action.type){
-    case("START_LOGIN_PROCESS"):
+    case("START_LOGIN_PROCESS" || "START_LOGOUT_PROCESS"):
       return {...state, requesting: true} 
     case("LOGIN_USER"):
-      return {...state, current_user: action.payload};
+      return {...state, current_user: action.payload, requesting: false};
     case("LOGOUT_USER"):
-      return {...state, current_user: {}};
+      return {...state, current_user: {}, requesting: false};
     default:
       return state;
   }
