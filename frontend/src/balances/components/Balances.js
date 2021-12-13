1e91f07e8a6df1807c8547b3def54fcdbda2cd16
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-13 14:54:45 -0500

    informs user

--- frontend/src/balances/components/Balances.js
+++ frontend/src/balances/components/Balances.js
@@ -3,8 +3,13 @@ import Balance from './Balance';
 
 const Balances = (props) => {
   const renderBalances = () => {
-
     const balances = props.balances.map(balance => <Balance balance={balance}/>)
+    if (balances.length < 1) {
+      return (<Balance balance={{
+        balance: "No Deposits Found"
+      }}/>)
+    }
+    else
       return balances
   }
   return (
