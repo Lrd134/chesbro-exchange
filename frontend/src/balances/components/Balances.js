932efb98b8ae02087c81ab198c2f970d7f28338b
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-13 11:48:43 -0500

    added balance

---
+++ frontend/src/balances/components/Balances.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Balances = (props) => {
+  const renderBalances = () => {
+
+  }
+  return (
+    <div>
+      {renderBalances()}
+    </div>
+  )
+}
+
+export default Balances;
