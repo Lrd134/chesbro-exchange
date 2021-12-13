c802c0c6ec529dbccf94a7b1693916222226b312
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-13 12:58:13 -0500

    validates email

--- frontend/src/users/components/UserForm.js
+++ frontend/src/users/components/UserForm.js
@@ -2,21 +2,31 @@ import './UserForm.css'
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { newUser } from '../actions/userFetch';
+import Signup from '../../login/components/Signup';
 class UserForm extends Component {
   state = {
     name: "",
     email: "",
+    validEmail: false
   }
   handleChange = event => {
-    this.setState({[event.target.name.split("-")[1]]: event.target.value})
+    function ValidateEmail(email) 
+{
+      if (email.match(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/))
+      {
+        return true
+      }
+        return false
+    }
+    const key = event.target.name.split("-")[1]
+    const value = event.target.value
+    if (key === "email")
+      this.setState({validEmail: ValidateEmail(event.target.value)})
+
+    this.setState({[key]: value})
   }
   handleSubmit = event => {
     event.preventDefault();
-    this.props.userEvent(this.state)
-    this.setState({
-      name: "",
-      email: ""
-    })
   }
 
   render() {
@@ -25,7 +35,7 @@ class UserForm extends Component {
         <form onSubmit={this.handleSubmit} className="user form">
           <input onChange={this.handleChange} name="user-name" type="text" className="user input"/><br></br>
           <input onChange={this.handleChange} name="user-email" type="text" className="user input"/><br></br><br></br>
-          <input type="submit" className="user submit"/>
+          <Signup user={this.state}/>
         </form>
       </div>
     )
