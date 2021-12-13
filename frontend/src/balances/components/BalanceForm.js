86fb755951a53e1d5f3bc474ca34ea5983e684d9
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-13 14:55:21 -0500

    +token dropdown

--- frontend/src/balances/components/BalanceForm.js
+++ frontend/src/balances/components/BalanceForm.js
@@ -4,8 +4,20 @@ import { connect } from 'react-redux';
 const BalanceForm = props => {
   return (
     <div>
+    <form  onSubmit={event => event.preventDefault()}>
+      <label>
+        Select Token to Deposit
+        <br></br>
+        <select name="value-token" onChange={props.handleChange}>
+          {renderOptions()}
+        </select>
+      </label>
+
+      <br>
+      </br>
 
     </div>
+    </form>
   )
 }
 
