028065c33d83add86a6dc697adb6f8e3ec4abefe
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-13 11:48:48 -0500

    added balance form

---
+++ frontend/src/balances/components/BalanceForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const BalanceForm = props => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+
+export default connect()(BalanceForm)
