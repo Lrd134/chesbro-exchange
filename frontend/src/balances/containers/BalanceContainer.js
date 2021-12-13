21d84324d39d1dad6fc5ee1584280c424adfb22f
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-13 13:09:08 -0500

    renders user balances

--- frontend/src/balances/containers/BalanceContainer.js
+++ frontend/src/balances/containers/BalanceContainer.js
@@ -4,13 +4,22 @@ import BalanceForm from '../components/BalanceForm';
 import Balances from '../components/Balances';
 
 class BalanceContainer extends Component {
+  balances = () => {
+    this.props.balances.balances.filter(balance => balance.user_id === this.props.current_user.current_user.id)
+  }
   render() {
     return (
       <div>
         <BalanceForm />
-        <Balances />
+        <Balances balances={this.balances()}/>
       </div>
     )
   }
 }
-export default connect()(BalanceContainer)
+const mapStateToProps = state => {
+  return {
+    balances: state.balances,
+    current_user: state.current_user
+  }
+}
+export default connect(mapStateToProps)(BalanceContainer)
