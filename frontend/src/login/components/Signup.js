0b51ed7b223894f31af451452998329c0a469ce6
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-13 12:59:00 -0500

    redirects optimistically

---
+++ frontend/src/login/components/Signup.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { newUser } from '../../users/actions/userFetch';
+const Signup = (props) => {
+  const navigate = useNavigate();
+  return (
+    <a href="Signup" alt="Signup" onClick={event => {
+      event.preventDefault();
+      if(props.user.validEmail) {
+        props.newUser(props.user);
+        navigate('/me');
+      }
+      else
+        alert("Invalid email entered.")
+    }}>Signup</a>
+  )
+}
+const mapStateToProps = state => {
+  return {
+    users: state.users,
+    current_user: state.current_user
+  }
+}
+export default connect(mapStateToProps, dispatch => ({newUser: user => newUser(dispatch, user)}))(Signup)
