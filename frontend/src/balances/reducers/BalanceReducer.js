bedeb674ba7b605104c46bed603bccec30be377e
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-13 16:21:50 -0500

    refactored new_balance

--- frontend/src/balances/reducers/BalanceReducer.js
+++ frontend/src/balances/reducers/BalanceReducer.js
@@ -9,16 +9,8 @@ const balanceReducer = (state = {
     {
       return {...state, balances: state.balances.concat(action.balances)}
     }
-    case("NEW_BALANCE"): {
-      const {name, email, id} = action.payload;
-      const balance = {
-        name,
-        email,
-        id
-      }
-      window.sessionStorage.setItem("logged_in", JSON.stringify({balance}))
-      
-      return {...state, balances: state.balances.concat(balance)}
+    case("NEW_BALANCE"): {      
+      return {...state, balances: state.balances.concat(action.payload.balance)}
     }
     case("REMOVE_BALANCE"):
       const remainingBalances = state.balances.filter(balance => balance.id !== action.payload)
