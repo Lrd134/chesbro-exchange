c0678a979135fba9fe7b8c4d48bb978361d7c791
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-15 17:43:58 -0500

    updated routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :tokens
+  resources :tokens, only: :show
   resources :balances
   resources :users
+  resources :orders
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
