b07541b29500b18aebaaceaefe083025a4ce77db
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-15 18:37:49 -0500

    can render a link

--- frontend/src/tokens/components/Token.js
+++ frontend/src/tokens/components/Token.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+const Token = ({link, token}) => {
+  const navigate = useNavigate();
+  if (link)
+    return (
+      <li>
+        <a href={`/exchange/${token.ticker}`} alt={token.ticker} onClick={event => {
+          event.preventDefault();
+          navigate(`/exchange/${token.ticker}`);
 
-const Token = ({token}) => {
-  return (
-    <li>
-      {token.ticker}
-    </li>
-  )
+        }}>{token.ticker}</a>
+      </li>
+    )
+  else
+    return (
+      <li>
+        {token.ticker}
+      </li>
+    )
 }
 
 export default Token;
