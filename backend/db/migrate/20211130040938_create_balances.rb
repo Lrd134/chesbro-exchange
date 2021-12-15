17d0d4a59c07ec6bd3b5e1b486536b9053042a39
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-15 17:45:31 -0500

    updated
    updated to make sure that balances do not receive an empty foreign key

--- backend/db/migrate/20211130040938_create_balances.rb
+++ backend/db/migrate/20211130040938_create_balances.rb
@@ -1,8 +1,8 @@
 class CreateBalances < ActiveRecord::Migration[6.1]
   def change
     create_table :balances do |t|
-      t.integer :user_id
-      t.integer :token_id
+      t.integer :user_id, null: false, foreign_key: true
+      t.integer :token_id, null: false, foreign_key: true
       t.float :balance
       t.timestamps
     end
