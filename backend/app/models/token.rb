512f57768521a3533cfaf9f562c036f52bab7a80
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-15 17:44:59 -0500

    updated models

--- backend/app/models/token.rb
+++ backend/app/models/token.rb
@@ -1,3 +1,4 @@
 class Token < ApplicationRecord
   has_many :balances
+  has_many :orders
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :balances
+  has_many :orders
   def self.find_by_any_params params
     params.match(/\D/) ? self.find_by_name(params) : self.find(params)
   end
