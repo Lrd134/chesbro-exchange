7a83587859637c1700f970133790f1dca8839340
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-16 14:12:45 -0500

    validates you have enough

--- backend/app/models/order.rb
+++ backend/app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
+  include ActiveModel::Validations
+  validates_with OrderValidator
   belongs_to :user
   belongs_to :token
   has_many :balances, through: :user
---
+++ backend/app/validators/order_validator.rb
@@ -0,0 +1,16 @@
+class OrderValidator < ActiveModel::Validator
+  def validate(record)
+    unless !record.sell
+      unless Balance.by_order record, record.user, record.token
+        record.errors.add :amount, "You don't have enough of that token."
+      end
+    end
+    
+    if !record.sell
+      unless Balance.by_order record, record.user
+        record.errors.add :amount, "Please deposit more USD";
+      end
+    end
+  end
+  
+end
