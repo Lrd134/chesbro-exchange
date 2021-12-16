1afd689bd85fe698661c5cead525601dfc1f87b6
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-16 00:59:25 -0500

    backend

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_12_15_220916) do
     t.bigint "user_id", null: false
     t.bigint "token_id", null: false
     t.float "amount"
+    t.boolean "sell"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["token_id"], name: "index_orders_on_token_id"
