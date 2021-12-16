e0c7b7cc4c3f707b4ca457338230482491774308
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-16 16:05:05 -0500

    shows data

--- frontend/src/orders/components/Orders.js
+++ frontend/src/orders/components/Orders.js
@@ -5,43 +5,25 @@ const Orders = props => {
   const params = useParams();
   const filterToken = () => props.tokens.filter(({attributes}) => attributes.ticker === params.ticker)
   const token = filterToken()[0];
-  const filterBuys = () => props.orders.filter(({attributes}) => attributes.token_id === token.attributes.id && attributes.sell === false )
-  const filterSells = () => props.orders.filter(({attributes}) => attributes.token_id === token.attributes.id && attributes.sell === true )
+  const filterBuys = () => props.orders.filter(({attributes}) => parseInt(attributes.token.id, 10) === parseInt(token.id, 10) && attributes.sell === false && !attributes.completed  )
+  const filterSells = () => props.orders.filter(({attributes}) => parseInt(attributes.token.id, 10) === parseInt(token.id, 10) && attributes.sell === true && !attributes.completed )
   const buys = filterBuys();
   const sells = filterSells();
-  const renderBuys = () => {
-    buys.map(({attributes}) => <td key={attributes.id} name="current-order">{attributes.amount}</td>)
-  }
-  const renderSells = () => {
-    sells.map(({attributes}) => <td key={attributes.id} name="current-order">{attributes.amount}</td>)
-  }
+  const renderBuys = () => buys.map(({attributes}) => <li key={attributes.id} name="current-order">{attributes.amount}</li>)
+  const renderSells = () => sells.map(({attributes}) => <li key={attributes.id} name="current-order">{attributes.amount}</li>)
   return (
     <div className="orders-container">
       <div className="buy-orders-container">
       <h1>Buy</h1>
-      <table>
-        <tr>
-          <th>
-            Amount
-          </th>
-        </tr>
-        <td>
+        <ul>
           {renderBuys()}
-        </td>
-      </table>
+        </ul>
       </div>
       <div className="sell-orders-container">
         <h1>Sells</h1>
-        <table>
-          <tr>
-            <th>
-              Amount
-            </th>
-          </tr>
-          <td>
-            {renderSells()}
-          </td>
-        </table>
+        <ul>
+          {renderSells()}
+        </ul>
       </div>
       
     </div>
