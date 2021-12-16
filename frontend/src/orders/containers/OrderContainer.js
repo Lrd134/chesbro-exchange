7a2642a298ea85e70e5df58d8543125f8a26a604
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-15 20:00:02 -0500

    updated
    updated to handle submitting orders

--- frontend/src/orders/containers/OrderContainer.js
+++ frontend/src/orders/containers/OrderContainer.js
@@ -3,6 +3,12 @@ import { connect } from 'react-redux';
 import orderActions from '../actions/orderActions';
 import Order from '../components/Order';
 class OrderContainer extends Component {
+  state = {
+    amounts: {
+      buy: 0,
+      sell: 0
+    }
+  }
   handleSell = ticker => {
     this.props.sellToken(this.state.amounts.sell, this.props.current_user.user.id, ticker)
   }
@@ -10,13 +16,31 @@ class OrderContainer extends Component {
 
   }
   handleChange = event => {
+    this.setState(prevState => ({
+      ...prevState,
+      amounts: {
+        ...prevState.amounts,
+        [event.target.name]: event.target.value
+      }
+    }))
   }
   render () {
-    return (
-      <div className='order-container'>
-        <Order handleSubmit={this.handleSubmit} handleChange={this.handleChange}/>
-      </div>
-    )
+    if (this.props.current_user.user) 
+      return (
+        <div className='order-container'>
+          <Order handleSell={this.handleSell} handleBuy={this.handleBuy} handleChange={this.handleChange} amounts={this.state.amounts} current_user={this.props.current_user}/>
+        </div>
+      )
+    else
+      return (
+        <h1> Please Login </h1>
+      )
   }
 }
-
+const mapStateToProps = ({current_user}) => ({
+  current_user: current_user.current_user
+})
+const mapDispatchToProps = dispatch => ({
+  sellToken: (amount, userId, tokenTicker) => dispatch(orderActions.newOrder(amount, userId, tokenTicker))
+})
+export default connect(mapStateToProps, mapDispatchToProps)(OrderContainer);
