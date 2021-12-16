442cf4d5b363759d91847cd10bcb21223d95680b
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-16 01:26:29 -0500

    finished order reducer

--- frontend/src/orders/reducers/OrderReducer.js
+++ frontend/src/orders/reducers/OrderReducer.js
@@ -14,3 +14,5 @@ const OrderReducer = (state = {
 
   }
 }
+
+export default OrderReducer;
--- frontend/src/root/reducers/RootReducer.js
+++ frontend/src/root/reducers/RootReducer.js
@@ -3,12 +3,14 @@ import UserReducer from '../../users/reducers/UserReducer';
 import TokenReducer from '../../tokens/reducers/TokenReducer';
 import BalanceReducer from '../../balances/reducers/BalanceReducer';
 import LoginReducer from '../../login/reducers/LoginReducer';
-
+import OrderReducer from '../../orders/reducers/OrderReducer.js'
 const RootReducer = combineReducers({
   users: UserReducer,
   balances: BalanceReducer,
   tokens: TokenReducer,
+  orders: OrderReducer,
   current_user: LoginReducer
+
 })
 
 export default RootReducer
