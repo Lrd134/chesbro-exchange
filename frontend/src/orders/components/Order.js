0b38a1c5ae615baf3969e1a376eb55a19e9a06be
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-15 20:00:11 -0500

    created order form

---
+++ frontend/src/orders/components/Order.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import SubmitOrder from './SubmitOrder';
+const Order = ({handleSell, handleBuy, handleChange, amounts}) => {
+  return (
+    <div className="order-component">
+      <form className="buy-form">
+          <input onChange={handleChange} name="buy" value={amounts.buy} type="number"/>
+          <SubmitOrder handleSubmit={handleBuy} text="Buy"/>
+      </form>
+      <form className="sell-form">
+          <input onChange={handleChange} name="sell" type="number" value={amounts.sell}/>
+          <SubmitOrder handleSubmit={handleSell} text={"Sell"}/>
+      </form>
+    </div>
+  )
+}
+
+export default Order;
