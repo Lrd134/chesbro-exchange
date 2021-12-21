88ee71eaaaa5064e8717abe742b98ece84832afa
Lrd134 <70249975+Lrd134@users.noreply.github.com>
2021-12-21 17:15:51 -0500

    made relationships dependent

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :balances
-  has_many :orders
+  has_many :balances, dependent: :destroy
+  has_many :orders, dependent: :destroy
   def self.find_by_any_params params
     params.match(/\D/) ? self.find_by_name(params) : self.find(params)
   end
